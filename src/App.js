33a05c6e7c80449c04dac4671ecc3af34bca20e9
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-19 15:14:40 +0100

    Import props from useFetch custom hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
+  //Destructure props from useFetch custom hook
+  const {loading, data} = useFetch();
   return <h2>pagination starter</h2>;
 }
 
