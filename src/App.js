599772ed83c6ccc77a8a2dffb2e378c229a8b980
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-19 15:21:59 +0100

    Map function

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
   //Destructure props from useFetch custom hook
-  const {loading, data} = useFetch();
-  return <h2>pagination starter</h2>;
+  const { loading, data } = useFetch();
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((follower)=> {
+            return  <Follower key={follower.id} {...follower}/>
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
 export default App;
