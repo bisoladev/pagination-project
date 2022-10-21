57e24f1e0b36221052a6b9157862563b5e78f403
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-21 18:26:04 +0100

    paginate functionality

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
-const paginate = () => {}
+const paginate = (followers) => {
+  const itemsPerPage = 8;
+  const pages = Math.ceil(followers.length/ itemsPerPage);
+
+  //The first thing we need to do when we set up a new array object is to specify the length, which in this case we are going to set it to our length object
+  const newFollowers = Array.from({length: pages}, (_, index)=> {
+    const start = index * itemsPerPage;
+    return followers.slice(start,start + itemsPerPage)
+  })
+
+  return newFollowers;
+}
 
 export default paginate
