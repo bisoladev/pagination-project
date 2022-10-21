760f8d6a7b45f7774f19dc3ab55e9514f1feef27
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-10-21 15:22:53 +0100

    Passed in data through the paginate function

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,20 +1,21 @@
-import { useState, useEffect } from 'react'
-import paginate from './utils'
-const url = 'https://api.github.com/users/john-smilga/followers?per_page=100'
+import { useState, useEffect } from "react";
+import paginate from "./utils";
+const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
 
 export const useFetch = () => {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
   const getProducts = async () => {
-    const response = await fetch(url)
-    const data = await response.json()
-    setData(data)
-    setLoading(false)
-  }
+    const response = await fetch(url);
+    const data = await response.json();
+    paginate(data);
+    setData(data);
+    setLoading(false);
+  };
 
   useEffect(() => {
-    getProducts()
-  }, [])
-  return { loading, data }
-}
+    getProducts();
+  }, []);
+  return { loading, data };
+};
