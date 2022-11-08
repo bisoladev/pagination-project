e1514b529e8da1526fb2dc042414a0e9f0683c05
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-08 16:59:02 +0100

    Prev and Next button functionality setup

--- src/App.js
+++ src/App.js
@@ -10,7 +10,31 @@ function App() {
   useEffect(() => {
     if (loading) return;
     setFollowers(data[page]);
-  }, [loading]);
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
   return (
     <main>
       <div className="section-title">
@@ -23,11 +47,27 @@ function App() {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
-        {!loading && <div className="btn-container">
-          {data.map((item, index)=> {
-            return <button className="page-btn"key={index}>{index + 1}</button>
-          })}
-          </div>}
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              Prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  key={index}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              Next
+            </button>
+          </div>
+        )}
       </section>
     </main>
   );
