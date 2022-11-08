f929f4db7b4142eb2a2820ba7c0dd71a1f797374
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-08 16:37:53 +0100

    Button functionality setup

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ function App() {
             return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && <div className="btn-container">
+          {data.map((item, index)=> {
+            return <button className="page-btn"key={index}>{index + 1}</button>
+          })}
+          </div>}
       </section>
     </main>
   );
